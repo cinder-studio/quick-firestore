78918111f1f60e1470db2493ba0b78085e5d4ad5
Ryan Hornberger <ryan.hornberger@mywhym.co>
2021-06-10 10:14:08 -0400

    Improving a few things.

--- src/FirestoreOverRest/testTools.ts
+++ src/FirestoreOverRest/testTools.ts
@@ -4,10 +4,10 @@ import  typedValues from "./typedValues"
 export { default as typedValues } from "./typedValues"
 
 // give the caller the ability to build a test document
-export const buildTestFsDocument = (options:{documentName:string, fields:any}) => {
+export const buildTestFsDocument = (options:{documentName?:string, fields:any}) => {
     return {
         "document": {
-            "name": options.documentName,
+            "name": options.documentName ? options.documentName : 'mock-firestore-document-name',
             ...typedValues.encodeDocument(undefined, options.fields),
             "createTime": "1970-01-01T00:00:01.000000Z", // hard-coded because using "now" in unit tests causes instability. We also don't recommend using these fields for actually tracking createdAt and updatedAt data.
             "updateTime": "1970-01-01T00:00:01.000000Z" // hard-coded because using "now" in unit tests causes instability. We also don't recommend using these fields for actually tracking createdAt and updatedAt data.
